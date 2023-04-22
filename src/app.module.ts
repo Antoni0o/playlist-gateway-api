28fd60d9cfda2f7074475cfcf2d6146e5f8219c5
Antonio <antonio.dias8997@gmail.com>
2023-04-22 09:47:40 -0300

    E-mail service config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,20 @@
+import { SendGridModule } from '@anchan828/nest-sendgrid';
 import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './common/services/typeorm.service';
 import { UsersModule } from './app/users/users.module';
-import { AuthsModule } from './app/auths/auths.module';
 
 @Module({
   imports: [
+    SendGridModule.forRoot({
+      apikey: process.env.SENDGRID_API_KEY,
+    }),
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
     }),
     UsersModule,
-    AuthsModule,
   ],
   controllers: [],
   providers: [Logger],
--- src/app/users/users.service.spec.ts
+++
@@ -1,3 +0,0 @@
-describe('Users Service', () => {
-  // To-do
-});
